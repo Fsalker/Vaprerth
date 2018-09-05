1be5aaa4b8fa27d92a9ce1dd5e47e62f77b0912e
Fsalker <andreiii500@yahoo.com>
2018-09-05 23:59:51 +0300

    heh

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = http.createServer(serverHandler);
 const PORT = process.env.PORT || 5000
 
 function serverHandler(req, res){
-    res.end("bomba");
+    res.end("le bomba");
 }
 
 server.listen(PORT);
